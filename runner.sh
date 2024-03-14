084f0d1594f76ce42fc4751db78af45fbd80666e
marcofpadeiro <2231953@my.ipleiria.pt>
2024-03-14 22:42:56 +0000

    Changing runner to run the rust

--- runner.sh
+++ runner.sh
@@ -3,6 +3,9 @@
 sleep=5 # minutes
 
 while true; do 
-    npx cypress run --spec "cypress/e2e/fetch.cy.js" && npx cypress run --spec "cypress/e2e/marcarPresenca.cy.js" && exit 0
+    echo "Running script"
+    ./target/release/gisem_rust
+
+    echo "Sleeping for $sleep minutes"
     sleep $((60*$sleep))
 done
