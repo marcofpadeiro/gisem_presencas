7a0591eaa7cc91f7a477ee6f69f1de6986afee5e
marcofpadeiro <2231953@my.ipleiria.pt>
2024-03-23 15:14:19 +0000

    Changing the structure of main.rs and exporting most of the logic to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use std::time::Duration;
+
+use fantoccini::{ClientBuilder, Locator};
+
+const URL_LOGIN: &str = "https://gisem.dei.estg.ipleiria.pt/login";
+const URL_PRESENCAS: &str = "https://gisem.dei.estg.ipleiria.pt/obterAulasMarcarPresenca";
+
 pub struct Config {
     pub username: String,
     pub password: String,
@@ -16,4 +23,120 @@ impl Config {
             rate,
         }
     }
+}
+
+pub async fn setup_client(
+    port: u16,
+) -> Result<fantoccini::Client, fantoccini::error::NewSessionError> {
+    let mut caps = serde_json::map::Map::new();
+    let opts = serde_json::json!({
+        "args": ["-headless"],
+    });
+    caps.insert("moz:firefoxOptions".to_string(), opts);
+
+    ClientBuilder::rustls()
+        .capabilities(caps)
+        .connect(format!("http://localhost:{}", port).as_str())
+        .await
+}
+
+pub async fn login(
+    c: &fantoccini::Client,
+    config: &Config,
+) -> Result<(), fantoccini::error::CmdError> {
+    // go to login page
+    c.goto(URL_LOGIN).await?;
+    let url = c.current_url().await?;
+    assert_eq!(url.as_ref(), URL_LOGIN);
+
+    // fill in the login form
+    c.find(Locator::Css("input[name=username]"))
+        .await?
+        .send_keys(config.username.as_str())
+        .await?;
+    c.find(Locator::Css("input[name=password]"))
+        .await?
+        .send_keys(config.password.as_str())
+        .await?;
+    c.find(Locator::Css("button[type=submit]"))
+        .await?
+        .click()
+        .await?;
+
+    Ok(())
+}
+
+pub async fn main_loop(
+    client: fantoccini::Client,
+    config: &Config,
+) -> Result<(), fantoccini::error::CmdError> {
+    let start = std::time::Instant::now();
+
+    // stops if the duration is reached or if the presences were already marked successfully
+    loop {
+        log!("Refreshing");
+        client.refresh().await?;
+
+        let link_result = client
+            .find(Locator::Css(
+                "a[href='https://gisem.dei.estg.ipleiria.pt/obterAulasMarcarPresenca']",
+            ))
+            .await;
+
+        if link_result.is_err() {
+            if start.elapsed().as_secs() / 60 >= config.duration as u64 {
+                return client.close().await;
+            }
+            log!("Presenças fechadas");
+
+            tokio::time::sleep(Duration::from_secs(config.rate as u64)).await;
+            continue;
+        }
+
+        link_result.unwrap().click().await?;
+
+        let url = client.current_url().await?;
+        assert_eq!(url.as_ref(), URL_PRESENCAS);
+
+        client
+            .find(Locator::Css("button[class='btn btn-primary col-xs-12']"))
+            .await?
+            .click()
+            .await?;
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        let message = client.get_alert_text().await?;
+
+        log!("{}", message);
+        if message.contains("sucesso")
+            || message.contains("Marcação de presença já foi efetuada anteriormente.")
+        {
+            return client.close().await;
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {{
+        use chrono::Local;
+        use std::fs::OpenOptions;
+        use std::io::Write;
+        use std::fs;
+
+        let now = Local::now();
+        let msg = format!("{} {}\n", now.format("[%Y-%m-%d %H:%M:%S]"), format_args!($($arg)*));
+        eprintln!("{}", msg);
+
+        fs::create_dir_all("logs").unwrap();
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(format!("logs/{}", now.format("%Y-%m-%d.log")))
+            .unwrap();
+
+        file.write_all(msg.as_bytes()).unwrap();
+    }};
 }
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,24 @@
-use std::time::Duration;
+use gisem_rust::{log, login, main_loop, setup_client, Config};
 
-use fantoccini::{ClientBuilder, Locator};
-use gisem_rust::Config;
-
-const URL_LOGIN: &str = "https://gisem.dei.estg.ipleiria.pt/login";
-const URL_PRESENCAS: &str = "https://gisem.dei.estg.ipleiria.pt/obterAulasMarcarPresenca";
+const PORT: u16 = 4444;
 
 #[tokio::main]
 async fn main() -> Result<(), fantoccini::error::CmdError> {
     let (duration, rate) = get_args();
     let config = Config::new(duration, rate);
 
-    let mut caps = serde_json::map::Map::new();
-    let opts = serde_json::json!({
-        "args": ["-headless"],
+    let client = setup_client(PORT).await.unwrap_or_else(|e| {
+        log!("Failed to create client: {}", e);
+        std::process::exit(1);
     });
-    caps.insert("moz:firefoxOptions".to_string(), opts);
 
-    let c = ClientBuilder::rustls()
-        .capabilities(caps)
-        .connect("http://localhost:4444")
-        .await
-        .expect("failed to connect to WebDriver");
-
-    login(&c, config.username, config.password).await?;
-
-    let start = std::time::Instant::now();
-
-    loop {
-        eprintln!("Refreshing");
-        c.refresh().await?;
-
-        let link_result = c
-            .find(Locator::Css(
-                "a[href='https://gisem.dei.estg.ipleiria.pt/obterAulasMarcarPresenca']",
-            ))
-            .await;
-
-        if link_result.is_err() {
-            if start.elapsed().as_secs() / 60 >= config.duration as u64 {
-                return c.close().await;
-            }
-            eprintln!("Presenças fechadas");
-            tokio::time::sleep(Duration::from_secs(config.rate as u64)).await;
-            continue;
-        }
-
-        link_result.unwrap().click().await?;
-
-        let url = c.current_url().await?;
-        assert_eq!(url.as_ref(), URL_PRESENCAS);
-
-        c.find(Locator::Css("button[class='btn btn-primary col-xs-12']"))
-            .await?
-            .click()
-            .await?;
-
-        tokio::time::sleep(Duration::from_secs(1)).await;
-
-        let message = c.get_alert_text().await?;
-
-        eprintln!("{}", message);
-        if message.contains("sucesso")
-            || message.contains("Marcação de presença já foi efetuada anteriormente.")
-        {
-            return c.close().await;
-        }
+    if let Ok(()) = login(&client, &config).await {
+        log!("Login as {} performed successfully!", config.username);
     }
-}
 
-async fn login(
-    c: &fantoccini::Client,
-    username: String,
-    password: String,
-) -> Result<(), fantoccini::error::CmdError> {
-    // go to login page
-    c.goto(URL_LOGIN).await?;
-    let url = c.current_url().await?;
-    assert_eq!(url.as_ref(), URL_LOGIN);
-
-    // fill in the login form
-    c.find(Locator::Css("input[name=username]"))
-        .await?
-        .send_keys(username.as_str())
-        .await?;
-    c.find(Locator::Css("input[name=password]"))
-        .await?
-        .send_keys(password.as_str())
-        .await?;
-    c.find(Locator::Css("button[type=submit]"))
-        .await?
-        .click()
-        .await?;
-
-    Ok(())
+    let res = main_loop(client, &config).await;
+    log!("");
+    res
 }
 
 fn get_args() -> (u32, u32) {
